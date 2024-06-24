301227efd42168c11101ceb696a5da5fad2e8a9f
Ksenia Ivanova <Brennendes.herz2507@mail.ru>
2024-06-24 14:46:13 +0300

    created func sort_by_date in processing.py

--- src/processing.py
+++ src/processing.py
@@ -6,3 +6,12 @@ def filter_by_state(data_list: list[dict], state: str = "EXECUTED") -> list[dict
         if data_dict["state"] == state:
             list_by_state.append(data_dict)
     return list_by_state
+
+
+def sort_by_date(data_list: list[dict], sort_reverse: bool = True) -> list[dict]:
+    """Возвращает новый список, в котором исходные словари отсортированы по датам"""
+
+    if sort_reverse:
+        return sorted(data_list, key=lambda x: ".".join(x["date"][:10].split("-")), reverse=True)
+    else:
+        return sorted(data_list, key=lambda x: ".".join(x["date"][:10].split("-")))
