903a69b437927cf7ec5dc3ad9182e7eb49c9cd2d
Ksenia Ivanova <Brennendes.herz2507@mail.ru>
2024-06-24 20:20:19 +0300

    processing.py change

--- src/processing.py
+++ src/processing.py
@@ -1,3 +1,6 @@
+from datetime import datetime
+
+
 def filter_by_state(data_list: list[dict], state: str = "EXECUTED") -> list[dict]:
     """Возвращает список операций, статус которых соответствует переданному в функцию"""
 
@@ -8,10 +11,9 @@ def filter_by_state(data_list: list[dict], state: str = "EXECUTED") -> list[dict
     return list_by_state
 
 
-def sort_by_date(data_list: list[dict], sort_reverse: bool = True) -> list[dict]:
+def sort_by_date(data_list: list[dict], is_sort_reverse: bool = True) -> list[dict]:
     """Возвращает новый список, в котором исходные словари отсортированы по датам"""
 
-    if sort_reverse:
-        return sorted(data_list, key=lambda x: ".".join(x["date"][:10].split("-")), reverse=True)
-    else:
-        return sorted(data_list, key=lambda x: ".".join(x["date"][:10].split("-")))
+    return sorted(
+        data_list, key=lambda x: datetime.strptime(x["date"], "%Y-%m-%dT%H:%M:%S.%f"), reverse=is_sort_reverse
+    )
