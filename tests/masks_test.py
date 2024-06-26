431d49d43b5f0230145eaa47943731b336b56f22
Ksenia Ivanova <Brennendes.herz2507@mail.ru>
2024-06-27 01:02:52 +0300

    removing unnecessary files

--- tests/masks_test.py
+++
@@ -1,8 +0,0 @@
-from src.masks import get_mask_account, get_mask_card_number
-
-if __name__ == "__main__":
-    input_card_num = input("Введите номер карты: ")
-    print(get_mask_card_number(input_card_num))
-
-    input_account_num = input("Введите номер счета: ")
-    print(get_mask_account(input_account_num))
