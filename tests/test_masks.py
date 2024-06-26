f1cd825d5fdcf29aeab1c00acbb061f03cf61fa4
Ksenia Ivanova <Brennendes.herz2507@mail.ru>
2024-06-27 01:20:13 +0300

    add tests for masks.py

---
+++ tests/test_masks.py
@@ -0,0 +1,12 @@
+import pytest
+
+from src.masks import get_mask_card_number, get_mask_account
+
+def test_get_mask_card_number():
+    assert get_mask_card_number("7000792289606361") == "7000 79** **** 6361"
+    assert get_mask_card_number("8569412532567854") == "8569 41** **** 7854"
+
+
+def test_get_mask_account():
+    assert get_mask_account("73654108430135874305") == "**4305"
+    assert get_mask_account("87458965541225635426") == "**5426"
