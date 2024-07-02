fd3981bc27174d2f187dc5f0bf5fdafe442ef0ff
Ksenia Ivanova <Brennendes.herz2507@mail.ru>
2024-07-02 17:02:22 +0300

    created func transaction_descriptions in generators.py

--- src/generators.py
+++ src/generators.py
@@ -1,7 +1,12 @@
 from typing import Iterable
 
 
-def filter_by_currency(transactions_data: list[dict], currency: str) -> Iterable[dict]:
+def filter_by_currency(transactions_data: list[dict], currency: str) -> Iterable:
     for transaction in transactions_data:
         if transaction["operationAmount"]["currency"]["code"] == currency:
             yield transaction
+
+
+def transaction_descriptions(transactions_data: list[dict]) -> Iterable:
+    for transaction in transactions_data:
+        yield transaction["description"]
