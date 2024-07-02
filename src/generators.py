9fd7468783e5a89f58e27c8f768cd3a7808dc6c4
Ksenia Ivanova <Brennendes.herz2507@mail.ru>
2024-07-02 22:09:01 +0300

    created func card_number_generator in generators.py

--- src/generators.py
+++ src/generators.py
@@ -10,3 +10,12 @@ def filter_by_currency(transactions_data: list[dict], currency: str) -> Iterable
 def transaction_descriptions(transactions_data: list[dict]) -> Iterable:
     for transaction in transactions_data:
         yield transaction["description"]
+
+
+def card_number_generator(init_value: int, final_value: int) -> Iterable:
+    if 1 <= init_value and init_value <= final_value <= 9999999999999999:
+        for num in range(init_value, final_value + 1):
+            r = str(num).zfill(16)
+            yield f"{r[:4]} {r[4:8]} {r[8:12]} {r[12:]}"
+    else:
+        yield "Введены некорректные значения"
